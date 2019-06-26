1cc45c4863426e9681606f9ae8c88bce16fc9b22
Race428 <raceacheson@gmail.com>
2019-06-26 12:29:57 -0600

    start of project

---
+++ src/InstaClone.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import 
+
+class InstaClone extends Component { 
+
+render(){
+    return(
+
+    )
+}
+
+}
+
+export default Component
+
+
+// stoppoed on this https://www.youtube.com/watch?time_continue=5&v=o7eB8wG20iw at 33:13
